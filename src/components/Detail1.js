bc7c0e0d009a41e4871ad13b15b09eaeb7f31a31
pokorra <patrycja.pokora@gmail.com>
2020-10-03 19:51:27 +0200

    sketch of Detail non-visible section

---
+++ src/components/Detail1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Detail1 = () => {
+
+    return (
+        <div className="detail">
+
+        </div>
+    )
+}
+
+export default Detail1;
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,14 +3,16 @@ import Title from './Title';
 import Content1 from './Content1';
 import Content2 from './Content2';
 import Content3 from './Content3';
+import Detail1 from './Detail1';
 
 const MainSection = () => {
 
 
     return (
-        <div className='mainsection'>
+        <div className='section'>
             <Title title='1) few things about me'/>
             <Content1/>
+            <Detail1/>
             <Title title="2) my skills and abilities"/>
             <Content2/>
             <Title title="3) projects i made"/>
