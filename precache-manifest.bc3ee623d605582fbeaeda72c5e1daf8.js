691f5cc865d3b2adfb0543655d8499825da5cfaa
pokorra <patrycja.pokora@gmail.com>
2020-10-09 13:52:11 +0200

    Updates

---
+++ precache-manifest.bc3ee623d605582fbeaeda72c5e1daf8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cedd7619743c9188f920519b6dd18c8",
+    "url": "/portfile/index.html"
+  },
+  {
+    "revision": "4a4407f23294ab941b7d",
+    "url": "/portfile/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "c4b615e425cc0ca9c2d9",
+    "url": "/portfile/static/css/main.07c9d45a.chunk.css"
+  },
+  {
+    "revision": "4a4407f23294ab941b7d",
+    "url": "/portfile/static/js/2.5a459251.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/portfile/static/js/2.5a459251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b615e425cc0ca9c2d9",
+    "url": "/portfile/static/js/main.4e48a401.chunk.js"
+  },
+  {
+    "revision": "43ee8f4d9df88c1b6579",
+    "url": "/portfile/static/js/runtime-main.d0b42762.js"
+  },
+  {
+    "revision": "60159cbeb137a02a5e9768ebc43d8f05",
+    "url": "/portfile/static/media/bestShop.60159cbe.png"
+  },
+  {
+    "revision": "6e2081580b3dbb3a697bfef0f2a22bd2",
+    "url": "/portfile/static/media/pflj.6e208158.jpg"
+  },
+  {
+    "revision": "322f613c3816efa0ecd069062f1c0c3a",
+    "url": "/portfile/static/media/pflm.322f613c.jpg"
+  },
+  {
+    "revision": "4c29782607aa3affbc4ea603bdd52367",
+    "url": "/portfile/static/media/quiz.4c297826.png"
+  },
+  {
+    "revision": "8ca8aefb2107045f084cdfea899b1b58",
+    "url": "/portfile/static/media/startHelping.8ca8aefb.png"
+  },
+  {
+    "revision": "e8cb986918dcbf4d32cbc3d80bd4010a",
+    "url": "/portfile/static/media/tasks.e8cb9869.png"
+  },
+  {
+    "revision": "241dc1ebab2183b6f879fc3aad8bd352",
+    "url": "/portfile/static/media/zombie.241dc1eb.png"
+  }
+]);
